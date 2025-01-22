7d0752d389d3db725de2d9c9949f11d734b3672d
shegaoyuan@aliyun.com <shegaoyuan@aliyun.com>
2025-01-22 12:23:38 +0800

    opt

--- files/lua/rpc/status/system.lua
+++ files/lua/rpc/status/system.lua
@@ -12,7 +12,7 @@ function M.get_info()
     local board_file = '/etc/board.json'
     data = fs.readfile(board_file)
     local board = cjson.decode(data)
-    local address_file = '/sys/class/net/'..board.network.wan.device..'/address'
+    local address_file = '/sys/class/net/' .. board.network.wan.device .. '/address'
     data = fs.readfile(address_file)
 
     return rpc.result_response('status/system', {
@@ -71,6 +71,29 @@ function M.get_memory()
         }
     })
 
+end
+
+function M.get_log()
+
+    local data = {}
+    local f = io.popen('logread -l 50', 'r')
+    if f then
+        for line in f:lines() do
+            local date, content = line:match("^(%a+ %a+ %d+ %d+:%d+:%d+ %d+) +(.+)$")
+            if date and content then
+                local entry = {
+                    date = date,
+                    content = content
+                }
+                table.insert(data, entry)
+            end
+        end
+        f:close()
+    end
+
+    return rpc.result_response('status/system', {
+        log = data
+    })
 end
 
 return M
